e23b0e3642862588c78d225b1f673ff311977e84
TomasMunozMoxey <66645497+TomasMunozMoxey@users.noreply.github.com>
2021-10-21 15:10:37 -0400

    Border around cover

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ $(document).ready(function(){
     $(window).scroll(function(){
         if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
+            $('.home').addClass("sticky");
         }else{
             $('.navbar').removeClass("sticky");
+            $('.home').removeClass("sticky");
         }
     })
 }); 
